cdba93f58ade0821d68698002dea46945f721327
ttymrz <tetsuya.morizumi@gmail.com>
2019-10-05 22:35:04 +0900

    change send intervalset to 15 minutes = 900 second

--- config/IoT_Sensor_Node_config.h
+++ config/IoT_Sensor_Node_config.h
@@ -9,7 +9,7 @@
 // <o> Send Interval <0-100000>
 // <i> Send interval in seconds
 // <id> application_send_interval
-#define CFG_SEND_INTERVAL 1
+#define CFG_SEND_INTERVAL 900
 
 // <o> Timeout <0-100000>
 // <i> Timeout
